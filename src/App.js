2ec1d771c7fa2f99060ba2e8f55655f57c29fa19
Achuchong <147917828+Achuchong@users.noreply.github.com>
2023-10-14 19:02:15 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'Jireh';
-  return <div>Hi People{value}</div>;
+  return <div>Hi People of the {value}</div>;
 }
 
 export default App;
